bfbd2b59d6b1129b6ec5d04e3019ecc6659af3b6
Kevin Paul <kevinpaul1100@gmail.com>
2020-08-02 15:57:32 +0530

    pycache

--- topDB.py
+++ topDB.py
@@ -4,19 +4,12 @@ import re
 
 def fun(e):
     return len(utf8.get_letters(e))
-      
 
- 
-path=""
-
-with open(path, newline='\n', encoding="utf-8") as f:
+with open("./combined_csv.csv", newline='\n', encoding="utf-8") as f:
     reader = csv.reader(f)
     print(reader)
     data = list(reader)
-    
-    
     data = [i[0] for i in data if(re.search("^[a-zA-Z0-9]*$",i[0])==None)]
     data = data[1:]
     data.sort(reverse=True,key=fun)
     print(data[0:1000])
-    
